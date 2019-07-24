ce0d310ba163a516f46e67ff30c9febf442c3509
Akha Manjezi <amanjezi@gmail.com>
2019-07-24 18:46:12 +0200

    moved LED update handling to main method and added code comments

--- Prac1.py
+++ Prac1.py
@@ -12,12 +12,12 @@ Date: 23/07/2019
 # import Relevant Librares
 import RPi.GPIO as GPIO
 
+#GPIO Set Up
 led_1 = 17 #physical pin 11
 led_2 = 23 #physical pin 16
 led_3 = 24 #physical pin 18
 button_up = 27 #physical pin 13
 button_down = 22 #physical pin 15
-
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(led_1, GPIO.OUT)
@@ -26,33 +26,38 @@ GPIO.setup(led_3, GPIO.OUT)
 GPIO.setup(button_up, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(button_down, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+#Binary Counter Valies
 values = ["000", "001", "010", "011", "100", "101", "110", "111"]
 global x
 x = 0
 
+#increments binary counter and updates LEDs
 def increment(channel):
     global x
     if x == 7:
         x = 0
     else:
         x += 1
-    GPIO.output(led_1, int(values[x][0]))
-    GPIO.output(led_2, int(values[x][1]))
-    GPIO.output(led_3, int(values[x][2]))
+    updateLEDs()
 
+#decrements binary counter and updates LEDs
 def decrement(channel):
     global x
     if x == 0:
         x = 7
     else:
         x -= 1
-    GPIO.output(led_1, int(values[x][0]))
-    GPIO.output(led_2, int(values[x][1]))
-    GPIO.output(led_3, int(values[x][2]))
+    updateLEDs()
 
+#adds event detectors for resective buttons
 GPIO.add_event_detect(button_up, GPIO.FALLING, callback=increment, bouncetime=200)
 GPIO.add_event_detect(button_down, GPIO.FALLING, callback=decrement, bouncetime=200)
 
+#updates LEDs
+def updateLEDs():
+    GPIO.output(led_1, int(values[x][0]))
+    GPIO.output(led_2, int(values[x][1]))
+    GPIO.output(led_3, int(values[x][2]))
 
 # Only run the functions if 
 if __name__ == "__main__":
